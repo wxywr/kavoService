8b90674cd74a66894749d66af51860330f087258
wxywr <1261046195@qq.com>
2017-11-02 11:01:41 +0800

    kavo

---
+++ js/inde.js
@@ -0,0 +1,13 @@
+$(window).resize(function(){
+    $('html').css({'font-size':$(window).width()/3.9+"px"});
+});
+
+$(function(){
+	$('html').css({'font-size':$(window).width()/3.9+"px"});
+	$('.fold').click(function(){
+		$('.accordion').slideToggle();
+	})
+	$('.submenu li').click(function(){
+		$(this).parent().parent().parent().css('display','none');
+	})
+});
